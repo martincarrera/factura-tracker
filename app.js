2f5c7b4e65f24f16496036827866c1aa436c8dbf
Martin Carrera <mcarrera.92@gmail.com>
2016-08-08 15:39:07 -0300

    Add array implementation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module("facturaTracker", ["firebase"]);
 
-app.controller("mainCtrl", function($scope, $firebaseObject) {
+app.controller("mainCtrl", function($scope, $firebaseObject, $firebaseArray) {
 
   var config = {
      apiKey: "AIzaSyDrZ4JH7jmMsJxuVVlGsb6CrUDGhojb03M",
@@ -19,4 +19,13 @@ app.controller("mainCtrl", function($scope, $firebaseObject) {
   // synchronize the object with a three-way data binding
   // click on `index.html` above to see it used in the DOM!
   syncObject.$bindTo($scope, "data");
+
+  ref = firebase.database().ref().child("messages");
+  $scope.messages = $firebaseArray(ref);
+
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
 });
