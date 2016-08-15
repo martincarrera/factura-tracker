04041ce7880fade7af785f9f18abc9aa93d490a4
Martin Carrera <mcarrera.92@gmail.com>
2016-08-14 23:45:51 -0300

    Implement login logic

--- js/controllers/login-controller.js
+++ js/controllers/login-controller.js
@@ -4,8 +4,22 @@
   .module('factura-tracker')
   .controller('LoginController', LoginController);
 
+  LoginController.$inject = [];
+
   function LoginController() {
     var vm = this;
+    vm.email = "";
+    vm.password = "";
+
+    vm.signInWithEmailAndPassword = () => {
+      firebase.auth().signInWithEmailAndPassword(vm.email, vm.password)
+      .then(data => {
+        if (data.uid) {
+          console.log('Usuario ' + data.email + ' logeado.');
+        }
+      });
+    }
+
   }
 
 })();
