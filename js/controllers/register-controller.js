accc1ed829705a6261669ef31abb06cdb8b01aa3
Martin Carrera <mcarrera.92@gmail.com>
2016-08-15 23:55:25 -0300

    Implement create account

--- js/controllers/register-controller.js
+++ js/controllers/register-controller.js
@@ -6,6 +6,16 @@
 
   function RegisterController() {
     var vm = this;
-  }
+    vm.email = "";
+    vm.password = "";
+
+    vm.createUserWithEmailAndPassword = () => {
+      firebase.auth().createUserWithEmailAndPassword(vm.email, vm.password)
+      .then(data => {
+        if (data.uid) {
+          console.log('Usuario ' + data.email + ' logeado.');
+        }
+      });
+    }  }
 
 })();
